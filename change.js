dd9ab1fd5720e2a5ebf8aa738e5a5de8e22f0c1d
HugoDoradea14 <00110418@uca.edu.sv>
2019-11-23 11:02:07 -0600

    iniciando

---
+++ change.js
@@ -0,0 +1,70 @@
+
+function show_dia1() {
+  var element = document.getElementById("d1");
+  var element_add0 = document.getElementById("intro");
+  var element_add1 = document.getElementById("d2");
+  var element_add2 = document.getElementById("d3");
+  var element_add3 = document.getElementById("d4");
+  element.classList.remove("hide");
+  element_add0.classList.add("hide");
+  element_add1.classList.add("hide");
+  element_add2.classList.add("hide");
+  element_add3.classList.add("hide");
+}
+
+
+function show_dia2() {
+    var element = document.getElementById("d2");
+    var element_add0 = document.getElementById("intro");
+    var element_add1 = document.getElementById("d1");
+    var element_add2 = document.getElementById("d3");
+    var element_add3 = document.getElementById("d4");
+    element.classList.remove("hide");
+    element_add0.classList.add("hide");
+    element_add1.classList.add("hide");
+    element_add2.classList.add("hide");
+    element_add3.classList.add("hide");
+  }
+
+
+function show_dia3() {
+    var element = document.getElementById("d3");
+    var element_add0 = document.getElementById("intro");
+    var element_add1 = document.getElementById("d1");
+    var element_add2 = document.getElementById("d2");
+    var element_add3 = document.getElementById("d4");
+    element.classList.remove("hide");
+    element_add0.classList.add("hide");
+    element_add1.classList.add("hide");
+    element_add2.classList.add("hide");
+    element_add3.classList.add("hide");
+  }
+
+
+function show_dia4() {
+    var element = document.getElementById("d4");
+    var element_add0 = document.getElementById("intro");
+    var element_add1 = document.getElementById("d1");
+    var element_add2 = document.getElementById("d2");
+    var element_add3 = document.getElementById("d3");
+
+    element.classList.remove("hide");
+    element_add0.classList.add("hide");
+    element_add1.classList.add("hide");
+    element_add2.classList.add("hide");
+    element_add3.classList.add("hide");
+  }
+
+
+function show_intro() {
+    var element = document.getElementById("intro");
+    var element_add0 = document.getElementById("d4");
+    var element_add1 = document.getElementById("d1");
+    var element_add2 = document.getElementById("d2");
+    var element_add3 = document.getElementById("d3");
+    element.classList.remove("hide");
+    element_add0.classList.add("hide");
+    element_add1.classList.add("hide");
+    element_add2.classList.add("hide");
+    element_add3.classList.add("hide");
+  }
